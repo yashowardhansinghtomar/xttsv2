549e134964ea41c0cac8ae7b99ae509885f3710c
yashowardhansinghtomar <108228223+yashowardhansinghtomar@users.noreply.github.com>
2025-02-19 10:44:35 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -12,6 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from pydub import AudioSegment
 from TTS.api import TTS
+torch.serialization.add_safe_globals([XttsConfig, XttsAudioConfig, BaseDatasetConfig, XttsArgs])
 
 # Initialize FastAPI
 app = FastAPI(
