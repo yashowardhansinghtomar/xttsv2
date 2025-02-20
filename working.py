611d0d13ad2b3c60be395aca03eef7607dce5558
yashowardhansinghtomar <108228223+yashowardhansinghtomar@users.noreply.github.com>
2025-02-20 18:25:35 +0530

    Update working.py

--- working.py
+++ working.py
@@ -190,4 +190,4 @@ async def generate_cloned_speech_endpoint(request: GenerateClonedSpeechRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, r
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
