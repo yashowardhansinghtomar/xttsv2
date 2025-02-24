5982793d542d1eb373b848ec7fc2d23d173243a4
yashowardhansinghtomar <108228223+yashowardhansinghtomar@users.noreply.github.com>
2025-02-24 15:00:28 +0530

    Update xttsv2A.py

--- xttsv2A.py
+++ xttsv2A.py
@@ -142,6 +142,6 @@ async def generate_cloned_speech(request: GenerateClonedSpeechRequest):
     with open(output_path, "rb") as audio_file:
         return Response(audio_file.read(), media_type=f"audio/{request.output_format}")
 
-if _name_ == "__main__":
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
